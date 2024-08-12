4ac1672047ccf695e538452b6d6a896718e230bd
Dat <phandinhd@outlook.com>
2024-08-12 10:18:53 -0700

    added hello world py file and practiced comments within the newly added 'beginner folder'

---
+++ beginner/comments.py
@@ -0,0 +1,5 @@
+# Hello World!
+# My name is Dat Phan
+# This is me commenting and utilizing the comment feature
+# a fun fact about me is I love snickerdoodle cookies
+# Python comments are very interesting and very useful for coding to specify or help other contributors/coders to your coding!
