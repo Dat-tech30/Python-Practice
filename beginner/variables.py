076fb4581872f55f5ebf8cc27d6aa3f077675d1e
Dat <phandinhd@outlook.com>
2024-08-12 11:27:47 -0700

    added variables in beginner folder

---
+++ beginner/variables.py
@@ -0,0 +1,20 @@
+# RUlES FOR NAMING VARIABLES
+
+# no spaces ex. the var = 21
+# no numbers at the start ex. 21variablename = 21 
+# no special characters except an underscore ex. variablename&? = 21
+
+# Examples of good naming -
+# use lowercase if possible
+
+the_var = 21
+var21 = 21
+var_special = 21
+
+greeting_you = "Hello! How are you today?"
+
+print(greeting_you)
+
+print(the_var)
+print(var21)
+print(var_special)
