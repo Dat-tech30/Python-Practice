077a2a14a70194447b5cfcc7a8e71c09a4940d83
Dat <phandinhd@outlook.com>
2024-08-14 12:52:54 -0700

    added function practice

---
+++ intermediate/functions.py
@@ -0,0 +1,32 @@
+# def function
+# the define function is reusable block of code, like a mathematical equation you always refer back or reuse every time
+# You first define it then call it
+
+def pets (): #have this colon
+    print('I love my dog!')
+    #Run your code
+    #Make sure to indent
+
+# then you call the function to run
+pets()
+
+# Rules for naming functions
+# No SPACES
+# No starting with NUMBERS
+# No special characters except _
+'''ex.
+
+def the func():
+def 21():
+def pets??():
+
+'''
+
+# Correct ways:
+
+# def the_func():
+    #code
+# def func21():
+    #code
+# def func():
+    #code
