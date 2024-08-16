120c5c1d1b28cce6927720212599aa93ae0af442
Dat <phandinhd@outlook.com>
2024-08-15 20:06:35 -0700

    practicing arguments and parameters

---
+++ intermediate/paremeters_arguments.py
@@ -0,0 +1,28 @@
+# The () function can be called for sending data to the function. This is called an argument
+# Parameters allow the function to accept the data
+# Parameters are the variables set by the arguments
+# parameter = "argument"
+
+
+def greet(greeting):
+    print(greeting)
+
+greet("Hello!")
+
+
+def values(par1, par2):
+    print(par1)
+    print(par2)
+
+values("1", "2")
+    
+
+# Multiple Parameters are utilized in through the first argument to first providing an initial value then it goes down to the 2nd parameter
+
+def pets(canine, feline):
+    print(canine)
+    print(feline)
+
+pets('dog', 'cat')
+
+# Arguments can be anything that has outputs or values!
