583e3252355026b736bd6c67489ecd2b520fe607
Dat <phandinhd@outlook.com>
2024-08-15 21:40:34 -0700

    practicing and reviewing

---
+++ intermediate/review.py
@@ -0,0 +1,37 @@
+# Practice Review!
+# This is an awesome comment!
+'''
+This is also an awesome comment!
+'''
+
+var1 = 21
+var_1 = "21"
+
+add1 = 1
+add2 = 2
+float1 = 222.10
+float2 = 21.22
+total =  add2 / add1
+
+string = str(float1)
+num1 = type(float1)
+
+print("Hello World")
+print(total)
+
+print("Hey the number is " + string)
+print(num1)
+
+print(f'Here are some numbers that I am print right now {add1}, {add2}, and the last one! {total}')
+print(r'''
+
+╭━━━╮┈┈╱╲┈┈┈╱╲
+┃╭━━╯┈┈▏▔▔▔▔▔▕
+┃╰━━━━━▏╭▆┊╭▆▕
+╰┫╯╯╯╯╯▏╰╯▼╰╯▕
+┈┃╯╯╯╯╯▏╰━┻━╯▕
+┈╰┓┏┳━┓┏┳┳━━━╯
+┈┈┃┃┃┈┃┃┃┃┈┈┈┈
+┈┈┗┻┛┈┗┛┗┛┈┈┈┈
+
+''')
