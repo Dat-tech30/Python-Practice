c31b9e95951aaa174c824e5872627d9810d767ff
Dat <phandinhd@outlook.com>
2024-08-16 21:05:18 -0700

    finished half way of conditional statement practice

--- hard/conditonal_statements.py
+++ hard/conditonal_statements.py
@@ -51,3 +51,42 @@ else:
 
 
 # Using the value equal
+
+chips = "BBQ"
+
+if chips == "BBQ":
+    print("Woah you got BBQ Chips? Can I have some?")
+else:
+    print("Oh it's not BBQ Nevermind.")
+
+# elif statements
+"""
+elif (short for "else if") is used to check additional conditions in a 
+sequence after an initial if statement and before an optional else statement, 
+allowing for multiple branches in conditional logic.
+"""
+
+videogames = "rpg"
+
+if videogames == "rpg":
+    print("Hey wanna play that rpg game?")
+elif videogames !="rpg":
+    print("ah it\'s alright we can play something else")
+elif videogames == "Food":
+    print("Let\'s just get some food!")
+else:
+    print("Nevermind I think I\'m gonna go home.")
+
+
+# We can also chain as much conditions as we need to!
+# nested if statements
+# nested if statements are if 2 conditions are true they can be together
+
+temperature = 70
+
+if temperature >= 70:
+    if temperature < 90:
+        print("Hey the weather is looking nice today! Lets go outside.")
+    else:
+        print("The weather is a little bit hot let\'s go the pool!")
+
