9bc6befacdf10aae4eecfdfbb8102b3ddeb55b98
Dat <phandinhd@outlook.com>
2024-08-16 19:23:55 -0700

    practicing conditional statements and added tensorflow practice as well

---
+++ hard/conditonal_statements.py
@@ -0,0 +1,53 @@
+# Boolean values are true or false statements
+# Boolean values in python are capitalized
+
+dog = True
+cat = False
+
+# if else statements are what they sound
+# conditional statements but in python to be used to find something is true or false given a value
+
+pet = False
+
+if pet:
+    print("This is a pet")
+else:
+    print("This is not a pet")
+
+pet2 = True
+
+if pet2:
+    print("This is a pet")
+else:
+    print("This is not a pet")
+
+
+# Conditional statements can get really specific if you need to check something
+# Comparison operators:
+'''
+greater than >
+less than <
+greater than or equal >=
+less than or equal <=
+value equal ==
+not equal !=
+'''
+
+chip_bags = 10
+
+if chip_bags >= 10:
+    print("That\'s a lot of chips you got there!")
+else:
+    print("You don\'t got that much chips.")
+
+
+chip_bags1 = 9
+
+if chip_bags1 >= 10:
+    print("That\'s a lot of chips you got there!")
+else:
+    print("You don\'t got that much chips.")
+
+
+
+# Using the value equal
