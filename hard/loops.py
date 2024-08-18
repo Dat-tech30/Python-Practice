c638ffd733b6be95a1483ce3ca3d136943860a4d
Dat <phandinhd@outlook.com>
2024-08-17 18:06:39 -0700

    practicing loops

---
+++ hard/loops.py
@@ -0,0 +1,25 @@
+# Loops
+# Loops are what they sound like, take a specific task to loop and keeping your code D.R.Y.
+# While loops repeats a block of code that is true until it becomes false
+# Each repeat is called an iteration
+
+soccer_balls = 0
+
+while soccer_balls <= 10:
+    print(soccer_balls)
+    soccer_balls = soccer_balls + 1
+
+
+# incrementing and decrementing
+"""
+num = 0
+num = += 1
+adds current value from 0
+
+
+num = 100
+num -= 10
+subtracts 10 from current value of 100
+"""
+
+
