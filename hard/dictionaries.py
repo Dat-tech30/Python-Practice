1583e5c1c9877e250249f8473f5a5e62f24ebc57
Dat <phandinhd@outlook.com>
2024-08-20 23:22:01 -0700

    started dictionary practice

---
+++ hard/dictionaries.py
@@ -0,0 +1,10 @@
+# Dictionaries are better versions of lists able to store thousands of data including able to key and value data
+# This means a keyed, mutable collection of values
+
+animals = {
+    "tiger" : "Jungle",
+    "Shark" : "Ocean",
+    "Frog" : "River",
+    "Scorpiopn" : "Desert",
+    "Polar Bear" : "Antartica"
+}
