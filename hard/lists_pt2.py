1d884c5d944f1bab94abae7e0377f803c341f1ac
Dat <phandinhd@outlook.com>
2024-08-20 23:24:34 -0700

    started lists practice

---
+++ hard/lists_pt2.py
@@ -0,0 +1,7 @@
+# The list() function is used to convert an iterable (such as a string, tuple or dictionary) into a list.
+
+# list()
+
+sports = list(('baseball', 'football', 'soccer', 'hockey', 'basketball'))
+
+print(sports)
