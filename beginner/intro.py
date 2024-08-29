7771d76bd0e078accf71b961cefa1cc3af93a5d5
Dat <phandinhd@outlook.com>
2024-08-29 16:40:22 -0700

    added floor division and cleaned up comment orientation

--- beginner/intro.py
+++ beginner/intro.py
@@ -1,18 +1,18 @@
-print(2 + 5) # Addition +
-print(5 - 2) # Subtraction -
-print(5 * 5) # multiplication *
-print(5 / 1) # division /
-print(3 ** 2) # exponential **
-print(3 % 2) # modulus %
-print() # Floor division operator //
+print(2 + 5)    # Addition +
+print(5 - 2)    # Subtraction -
+print(5 * 5)    # multiplication *
+print(5 / 1)    # division /
+print(3 ** 2)   # exponential **
+print(3 % 2)    # modulus %
+print(3 // 2)   # Floor division operator //
 
 # Checking data types
 
 print(type(12))                  # Int
 print(type(2.11))                # Float
 print(type(2 + 4j))              # Complex
-print(type('Dat'))          # String
+print(type('Dat'))               # String
 print(type([1, 2, 3]))           # List
-print(type({'name':'Dat'})) # Dictionary
+print(type({'name':'Dat'}))      # Dictionary
 print(type({9.2, 2.11, 2.5}))    # Set
 print(type((6.8, 2.11, 2.7)))    # Tuple
