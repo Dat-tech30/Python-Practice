6351362fc93aaab1493554a9adb8814d2ee0ec2d
Dat <phandinhd@outlook.com>
2024-08-29 20:37:54 -0700

    finished practice

--- more_practice/variables_builtin_functions.py
+++ more_practice/variables_builtin_functions.py
@@ -1,5 +1,5 @@
-first_name = 'dat'
-last_name = 'phan'
+first_name = 'Dat'
+last_name = 'Phan'
 state = 'washington'
 age = '500'
 is_married = 'false'
@@ -12,3 +12,25 @@ info = {
 }
 
 # Printing the values stored in the variables
+
+
+print('First name:', first_name)
+print('First name length:', len(first_name))
+print('Last name: ', last_name)
+print('Last name length: ', len(last_name))
+print('state: ', state)
+print('age: ', age)
+print('Married: ', is_married)
+print('Info: ', info)
+
+
+# Declaring multiple variables in one line
+
+first_name, last_name, state, age, is_married = 'Dat', 'Phan', 'Washington', 500, False
+
+print(first_name, last_name, state, age, is_married)
+print('First name:', first_name)
+print('Last name: ', last_name)
+print('State: ', state)
+print('Age: ', age)
+print('Married: ', is_married)
