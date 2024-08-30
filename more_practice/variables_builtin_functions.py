23faa14a5b6acfc0d7e7e4a266dbe0f4d6a79d2a
Dat <phandinhd@outlook.com>
2024-08-29 18:33:26 -0700

    working on built in variables practice

---
+++ more_practice/variables_builtin_functions.py
@@ -0,0 +1,14 @@
+first_name = 'dat'
+last_name = 'phan'
+state = 'washington'
+age = '500'
+is_married = 'false'
+hobbies = ['anime', 'videogames', 'music', 'driving', 'food']
+info = {
+    'age': '500',
+    'eye-color': 'black',
+    'state': 'WA',
+    'height': '8\'0 ft'
+}
+
+# Printing the values stored in the variables
