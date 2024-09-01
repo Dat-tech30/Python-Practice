ca85da75fbfdc253974f7b3cfb16b73d806ef7e2
Dat <phandinhd@outlook.com>
2024-08-31 22:36:26 -0700

    added returning substrings, unpacking characters

--- more_practice/more_strings.py
+++ more_practice/more_strings.py
@@ -19,3 +19,24 @@ print(last_three)   # hon
 fruit = "banana"
 slicing_fruit = fruit[0:2]
 print('I sliced the banana into', slicing_fruit)
+
+
+# returing the index of first occurence of substring
+
+fruits = "Apples, Oranges, Watermelon"
+print(fruits.find('a'))
+print(fruits.find('A'))
+
+
+# Unpacking characters
+
+coding_language = "JavaScript"
+
+a, b, c, d, e, f, g, h, i, j, = coding_language
+print(a)
+
+print(f'in the word {coding_language} there is the letter', a ,'in it')
+
+
+
+# There is so many strings methods and functions - I can't cover all of them however if I would like to check it out I can go to geekforgeeks or look it up!
