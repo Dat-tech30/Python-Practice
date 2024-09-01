c8f5d4df30a3cb5fe530a972b685a4202bfe85c6
Dat <phandinhd@outlook.com>
2024-08-31 20:56:02 -0700

    added more practice with operators and the length function

--- more_practice/intro.py
+++ more_practice/intro.py
@@ -16,3 +16,42 @@ print(type([1, 2, 3]))           # List
 print(type({'name':'Dat'}))      # Dictionary
 print(type({9.2, 2.11, 2.5}))    # Set
 print(type((6.8, 2.11, 2.7)))    # Tuple
+
+
+
+
+
+"""
+---------------------------------------------------------------SECTION--------------------------------------------------------------------------------------------------------------
+"""
+
+print('Addition: ', 1+2)
+print('Floating Number, PI', 3.14)
+
+
+angle_90 = 45
+
+print('Right Triangle is', angle_90 + angle_90, "degrees")
+
+print(3==2)
+print(3==3)
+print(3 > 2)
+print(3 < 2)
+print(3 >= 2)   
+print(2 <= 3) 
+
+
+
+print('The number of characters in the chosen word is the same as the 2nd word:', len('banana') == len('phone'))
+
+"""
+len('banana') == len('phone'))
+
+you can use different operators such as:
+< less than
+> greater too
+<= greater than or equal too
+>= less than or equal too
+
+"""
+
