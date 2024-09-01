7cf49f41969de7b48ef0545dee49b6e0b9dcfe46
Dat <phandinhd@outlook.com>
2024-08-31 21:10:29 -0700

    more in depth string practice

---
+++ more_practice/more_strings.py
@@ -0,0 +1,21 @@
+# Ex.
+# Slicing Python strings
+language = 'Python'
+first_three = language[0:3] # starts at zero index and up to 3 but not include 3
+print(first_three) #Pyt
+last_three = language[3:6]
+print(last_three) # hon
+# Another way
+last_three = language[-3:]
+print(last_three)   # hon
+last_three = language[3:]
+print(last_three)   # hon
+
+
+
+
+# Practice
+
+fruit = "banana"
+slicing_fruit = fruit[0:2]
+print('I sliced the banana into', slicing_fruit)
