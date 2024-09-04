999c2541e1033776a5a0460dba4ddf4d36dd0bc5
Dat <phandinhd@outlook.com>
2024-09-03 20:27:04 -0700

    compiling tensorflow and optimizing it to search on the end outcome for churning

--- tensorflow.py
+++ tensorflow.py
@@ -1,6 +1,7 @@
 '''import tensorflow as tf
 print("TensorFlow version:", tf.__version__)'''
 
+
 import pandas as pd
 from sklearn.model_selection import train_test_split
 
@@ -15,3 +16,12 @@ y_train.head()
 from tensorflow.keras.models import Sequential, load_model
 from tensorflow.keras.layers import Dense
 from sklearn.metrics import accuracy_score
+
+model = Sequential()
+model.add(Dense(units=32, activation='relu', input_dim=len(X_train.columns)))
+model.add(Dense(units=64, activation='relu'))
+model.add(Dense(units=1, activation='sigmoid'))
+
+model.compile(loss='binary_crossentropy', optimizer='sgd', metrics='accuracy')
+
+model.fit(X_train, y_train, epochs=200, batch_size=32)
