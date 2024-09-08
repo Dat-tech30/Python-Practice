6dd427e6edbdbbe378f974cb17dc5b4b9ce9fbbe
Dat <phandinhd@outlook.com>
2024-09-07 22:32:22 -0700

    list practicing and data collection types in python

---
+++ more_practice/lists.py
@@ -0,0 +1,10 @@
+# 4 types of data collection types in python
+# List: ordered and changeable(modifiable) values into a collection data type, it allows duplicate members
+# Tuple: same thing as a list however it is unchangeable or unmodifiable(immutable)
+# Set: a unordered, un-indexed and unmodifiable
+# Dictionary: unordered, changeable and indexed. No duplicate members
+
+# These data collection types are used and referred to store values in large scale to be used again and again. Say for example hundreds of thousands of employees information in a data base to be used.
+
+empty_list = list() # no item in the list
+print(len(empty_list)) # 0
