63cbf82eae603fcd603d4a8980d489c9e1a680a2
Dat <phandinhd@outlook.com>
2024-09-17 23:45:34 -0700

    practice returning values

---
+++ more_practice/higher_order_functions.py
@@ -0,0 +1,29 @@
+def sum_numbers(nums):  # normal function
+    return sum(nums)    # a sad function abusing the built-in sum function
+
+def higher_order_function(f, lst):  # function as a parameter
+    summation = f(lst)
+    return summation
+result = higher_order_function(sum_numbers, [1, 2, 3, 4, 5])
+print(result)       # 15
+
+
+def square(x):          # a square function
+    return x ** 2
+
+def cube(x):            # a cube function
+    return x ** 3
+
+def absolute(x):        # an absolute value function
+    if x >= 0:
+        return x
+    else:
+        return -(x)
+
+def higher_order_function(type): # a higher order function returning a function
+    if type == 'square':
+        return square
+    elif type == 'cube':
+        return cube
+    elif type == 'absolute':
+        return absolute
